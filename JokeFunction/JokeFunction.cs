e6ee7d9752981469dbfaf24d385b51955e9f729d
Grant Erickson <grant@granterickson.com>
2023-11-02 15:59:52 -0700

    Get Joke is now random.

--- JokeFunction/JokeFunction.cs
+++ JokeFunction/JokeFunction.cs
@@ -8,6 +8,8 @@ using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos;
+using System.Linq;
 
 namespace JokeFunction
 {
@@ -19,9 +21,13 @@ namespace JokeFunction
             [CosmosDB(
                 databaseName: "Jokes",
                 containerName: "items",
-                Connection = "CosmosDBConnection",
-                Id = "{Query.id}",
-                PartitionKey = "{Query.partitionKey}")]Joke joke,
+                    Connection = "CosmosDBConnection")] CosmosClient client,
+            //[CosmosDB(
+            //    databaseName: "Jokes",
+            //    containerName: "items",
+            //    Connection = "CosmosDBConnection",
+            //    Id = "{Query.id}",
+            //    PartitionKey = "{Query.partitionKey}")]Joke joke,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -34,12 +40,43 @@ namespace JokeFunction
 
             // var js = new JokeService(log);
             // var joke = js.GetRandomJoke(search);
+            Container container = client.GetDatabase("Jokes").GetContainer("items");
+
+            log.LogInformation($"Searching for Joke Count");
+
+            QueryDefinition queryDefinition = new QueryDefinition(
+                "SELECT value Count(i) FROM items i");
+
+            int count = 0;
+            using (FeedIterator<int> resultSet = container.GetItemQueryIterator<int>(queryDefinition))
+            {
+                count = (await resultSet.ReadNextAsync()).First();
+            }
+            log.LogInformation($"{count} jokes found");
+
+            // Random number between 0 and count
+            var rnd = new Random();
+            int offset = rnd.Next(count);
+
+            log.LogInformation($"Grabbing joke {offset} of {count}");
+
+
+            QueryDefinition queryDefinitionJoke = new QueryDefinition(
+                $"SELECT * FROM items i OFFSET {offset} LIMIT 1");
+
+            Joke? joke = null;
+            using (FeedIterator<Joke> resultSet = container.GetItemQueryIterator<Joke>(queryDefinitionJoke))
+            {
+                joke = (await resultSet.ReadNextAsync()).First();
+            }
+
+
             if (joke == null)
             {
                 return new OkObjectResult($"No jokes found with Joke Id {req.Query["id"]} and author {req.Query["partitionKey"]}");
             }
             return new OkObjectResult(joke);
-            
+
         }
     }
 }
